68a2f80910ac54d135e19a50ac8f7a3cde2acc24
Jon <jonmelnick@hotmail.com>
2017-01-27 15:17:30 -0800

    9: Security with methods

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,22 +1,19 @@
 import React, { Component, PropTypes } from 'react';
-
-import { Tasks } from '../api/tasks.js';
+import { Meteor } from 'meteor/meteor';
 
 export default class Task extends Component {
   toggleChecked(){
-    Tasks.update(this.props.task._id, {
-      $set: { checked: !this.props.task.checked },
-    });
+    Meteor.call('tasks.setChecked', this.props.task._id, !this.props.task.checked)
   }
 
   deleteThisTask(){
-    Tasks.remove(this.props.task._id);
+    Meteor.call('tasks.remove', this.props.task._id);
   }
 
   render() {
 
     const taskClassName = this.props.task.checked ? 'checked' : '';
-
+    console.log(this.props.task);
     return (
       <li className={taskClassName}>
         <button className="delete"
@@ -31,7 +28,9 @@ export default class Task extends Component {
         />
 
         <span className='text'>
-          <strong>{this.props.task.username || 'unkown'}</strong>: {this.props.task.text}
+          <strong>
+            {this.props.task.username || 'unkown'}
+          </strong>: {this.props.task.text}
         </span>
       </li>
     );
